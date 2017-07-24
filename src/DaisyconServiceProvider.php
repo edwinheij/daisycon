02ca7b3f8c0d1be0dad85c9ca8d0b3b26b9cbf88
Edwin Heij <e.heij@konnectit.nl>
2017-07-25 01:46:27 +0200

    Cleared service provider

--- src/DaisyconServiceProvider.php
+++ src/DaisyconServiceProvider.php
@@ -20,10 +20,6 @@ class DaisyconServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-//        $this->addPublishGroup('config', [
-//            __DIR__ . '/config/daisycon.php' => config_path('daisycon.php'),
-//        ]);
-
         $this->loadMigrationsFrom([
             __DIR__ . '/database/migrations',
         ]);
@@ -32,12 +28,7 @@ class DaisyconServiceProvider extends ServiceProvider
             __DIR__ . '/config/daisycon.php' => config_path('daisycon.php'),
         ], 'config');
 
-//        $this->mergeConfigFrom(
-//            __DIR__ . '/config/daisycon.php', 'daisycon'
-//        );
-
         $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
-
     }
 
     /**
@@ -47,57 +38,22 @@ class DaisyconServiceProvider extends ServiceProvider
      */
     public function register()
     {
-
-//        $this->app['daisycon.get-feeds'] = $this->app->singleton('DaysiconGetFeeds', function () {
-//            return new Commands\DaisyconGetFeeds();
-//        });
-
-        /*$this->app['daisycon.get-subscriptions'] = $this->app->share(function () {
-            return new Commands\DaisyconGetSubscriptions();
-        });
-
-        $this->app['daisycon.get-programs'] = $this->app->share(function () {
-            return new Commands\DaisyconGetPrograms();
-        });
-
-        $this->app['daisycon.fix-data'] = $this->app->share(function () {
-            return new Commands\DaisyconFixData();
-        });*/
-
-//        $this->app['daisycon.import-data'] = $this->app->share(function () {
         $this->app->register('Maatwebsite\Excel\ExcelServiceProvider');
         $this->app->alias('Excel', 'Maatwebsite\Excel\Facades\Excel');
 
-        $feed_type = ucfirst(strtolower(Config::get('daisycon.feed_type', 'Csv')));
-
-//			$this->app->bind('Bahjaat\Daisycon\Repository\DataImportInterface', 'Bahjaat\Daisycon\Repository\\Raw'.$feed_type.'DataImport');
-        if ($feed_type == 'Csv') {
             $this->app->bind(
                 'Bahjaat\Daisycon\Repository\DataImportInterface',
-                'Bahjaat\Daisycon\Repository\\League' . $feed_type . 'DataImport'
+                'Bahjaat\Daisycon\Repository\\XmlDataImport'
             );
-        } elseif ($feed_type == 'Xml') {
-            $this->app->bind(
-                'Bahjaat\Daisycon\Repository\DataImportInterface',
-                'Bahjaat\Daisycon\Repository\\'.$feed_type.'DataImport'
-            );
-        }
 
         $this->app->make('Bahjaat\Daisycon\Repository\DataImportInterface');
-//            $dataImportInterface = $this->app->make('Bahjaat\Daisycon\Repository\DataImportInterface');
-//            return new Commands\DaisyconImportData($dataImportInterface);
-//        });
+
         $this->commands([
             DaisyconGetFeeds::class,
             DaisyconGetPrograms::class,
             DaisyconGetSubscriptions::class,
             DaisyconImportData::class,
             DaisyconFixData::class
-//            'daisycon.get-feeds',
-//            'daisycon.get-subscriptions',
-//            'daisycon.import-data',
-//            'daisycon.get-programs',
-//            'daisycon.fix-data'
         ]);
     }
 }
