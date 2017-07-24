08b00f128dceb8f6ad2a559de453f52ce19b4114
Edwin Heij <e.heij@konnectit.nl>
2017-07-25 01:46:49 +0200

    Models updated

--- src/Models/Feed.php
+++ src/Models/Feed.php
@@ -7,20 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Feed extends Model
 {
 
-    /*protected $fillable = [
-        'feed_id',
-        'name',
-        'program_id',
-        'product_count',
-        'last_update',
-        'feed_link_csv',
-        'feed_link_xml',
-        'feed_link_xmlatt',
-        'feed_link_csv_update',
-        'feed_link_xml_update',
-        'feed_link_xmlatt_update',
-    ];*/
-
     protected $guarded = [];
 
     protected $casts = [
--- src/Models/Subscription.php
+++ src/Models/Subscription.php
@@ -5,5 +5,24 @@ namespace Bahjaat\Daisycon\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Subscription extends Model {
-	protected $fillable = ['program_id', 'advertiser_id', 'media'];
+
+    protected $guarded = [];
+
+    public function setSubscribeDateAttribute($value)
+    {
+        if ($value == '0000-00-00 00:00:00') {
+            $this->attributes['subscribe_date'] = null;
+        } else {
+            $this->attributes['subscribe_date'] = $value;
+        }
+    }
+
+    public function setApprovalDateAttribute($value)
+    {
+        if ($value == '0000-00-00 00:00:00') {
+            $this->attributes['approval_date'] = null;
+        } else {
+            $this->attributes['approval_date'] = $value;
+        }
+    }
 }
